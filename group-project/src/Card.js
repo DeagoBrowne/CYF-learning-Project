4bda630863419b2b0654fd43dca27acc7fc71d45
Ika Khoza <ikaneng.khoza@gmail.com>
2022-05-21 13:05:56 +0100

    changed card
    Changed card

--- group-project/src/Card.js
+++ group-project/src/Card.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Card = () => {
+  const [thingsLearned, setThingsLearned] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-learnings.glitch.me/coding")
+      .then((res) => res.json())
+      .then((data) => setThingsLearned(data));
+  }, []);
 
   return (
-    <div className="card">
-
+    <div className="container">
+      {thingsLearned.map((item, index) => {
+        return (
+          <div key={index} className="card">
+            <h1>{item.fullName}</h1>
+            <p>{item.ideas}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Card;
